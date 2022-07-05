b5486447125eba701091126df16fce7a2a4e68f0
mkbozai <munaaf24@gmail.com>
2022-07-05 07:20:14 -0500

    next commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,60 @@
 /*----- constants -----*/
-
+const COLORS = {
+    '0': 'white',
+    '1': 'red',
+    '-1': 'yellow'
+}
 
 /*----- app's state (variables) -----*/
 let board; // nested arrays represent columns
-let turn; // 1 or -1, null for empty slot
+let turn; // 1 or -1
 
 /*----- cached element references -----*/
-
+const markerEls = [...document.querySelectorAll('#markers > div')];
 
 /*----- event listeners -----*/
-
+document.getElementById('markers').addEventListener('click', handleDrop);
 
 /*----- functions -----*/
 init();
 
 function init() {
     board = [
-        [null, null, null, null, null, null], // column 1
-        [null, null, null, null, null, null], // column 2 
-        [null, null, null, null, null, null], // column 3
-        [null, null, null, null, null, null], // column 4
-        [null, null, null, null, null, null], // column 5
-        [null, null, null, null, null, null], // column 6
-        [null, null, null, null, null, null], // column 7
+        [0, 0, 0, 0, 0, 0], // column 0
+        [0, 0, 0, 0, 0, 0], // column 1
+        [0, 0, 0, 0, 0, 0], // column 2
+        [0, 0, 0, 0, 0, 0], // column 3
+        [0, 0, 0, 0, 0, 0], // column 4
+        [0, 0, 0, 0, 0, 0], // column 5
+        [0, 0, 0, 0, 0, 0], // column 6
     ];
     turn = 1;
     render();
 }
 
 function render() {
+    board.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(cirVal, rowIdx){
+            const cirEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+            cirEl.style.backgroundColor = COLORS[cirVal];
+        });
+    });
+    renderMarkers();
+}
 
+// hide/show the markers (hide if no 0's exist in that column)
+function renderMarkers() {
+    markerEls.forEach(function(markerEl, colIdx) {
+        markerEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
+    });
+}
+
+function handleDrop(evt) {
+    const colIdx = markerEls.indexOf(evt.target);
+    if (colIdx === -1) return;
+    const colArr = board[colIdx];
+    const rowIdx = colArr.indexOf(0);
+    colArr[rowIdx] = turn;
+    turn *= -1;
+    render();
 }
